99d5b5887c2238b52381ce655f95a6cc52361067
molemma <ml2cc@virginia.edu>
2021-07-07 19:10:07 -0400

    comparisons between EJSCREEN and replication data

--- ejscreen/ejscreen.R
+++ ejscreen/ejscreen.R
@@ -1,8 +1,10 @@
-# This is the 2020 EJSCREEN data
+# Exploration of the 2020 EJSCREEN data
+# The 2020 data uses 2016 estimates for PM2.5, which makes it an okay comparison for the replication data
 
 library(tidyverse)
 library(leaflet)
 library(sf)
+library(stargazer)
 
 # Load data
 ejscreen <- read_csv("/Users/marisalemma/Downloads/EJSCREEN_2020_USPR.csv")
@@ -14,7 +16,7 @@ ejscreen_clean <- ejscreen %>%
             "T_LDPNT_D2", "T_DSLPM_D2", "T_CANCR_D2", "T_RESP_D2", "T_PTRAF_D2", "T_PWDIS_D2", 
             "T_PNPL_D2", "T_PRMP_D2", "T_PTSDF_D2", "T_OZONE_D2", "T_PM25_D2", "Shape_Length", "Shape_Area"))
 
-# Filter to just Cville area
+# Filter to just areas of interest
 virginia <- ejscreen_clean %>% 
   filter(STATE_NAME=="Virginia")
 cville_area <- virginia %>% 
@@ -24,7 +26,9 @@ cville_area <- virginia %>%
            ID%in%511099501001:511099505003 |
            ID%in%511259501001:511259503004 |
            ID%in%515400002011:515400010003)
-
+eastern_shore <- virginia %>% 
+  filter(ID%in%510010901001:510019902000 |
+            ID%in%511319301001:511319901000)
 
 # Some spatial explorations
 # Load shapefile
@@ -67,22 +71,132 @@ leaflet(cvilleshapes) %>%
   addLegend("bottomright", pal = pal, values = cvilleshapes$PTRAF,
             title = "Traffic Proximity", opacity = 0.7)
 
+# Spatial distribution of PM2.5 percentile
+pal <- colorNumeric("plasma", reverse = TRUE, domain = cvilleshapes$P_PM25)
+leaflet(cvilleshapes) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = cvilleshapes,
+              fillColor = ~pal(P_PM25),
+              weight = 1,
+              opacity = 1,
+              color = "white",
+              fillOpacity = 0.6,
+              highlight = highlightOptions(weight = 2, fillOpacity = 0.8, bringToFront = T),
+              popup = paste0("FIPS Code: ", cvilleshapes$GEOID, "<br>",
+                             "PM2.5 Percentile: ", cvilleshapes$P_PM25)) %>%
+  addLegend("bottomright", pal = pal, values = cvilleshapes$P_PM25,
+            title = "PM2.5 Percentiles", opacity = 0.7)
+# I compared these percentiles to the ones from the air quality replication data and they are very different
+  # The trends are not even the same - the Cville tracts are the highest percentile in the replication data,
+    # but in this data the highest percentile is in Louisa County
+
+# Spatial distribution of PM2.5
+pal <- colorNumeric("plasma", reverse = TRUE, domain = cvilleshapes$PM25)
+leaflet(cvilleshapes) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = cvilleshapes,
+              fillColor = ~pal(PM25),
+              weight = 1,
+              opacity = 1,
+              color = "white",
+              fillOpacity = 0.6,
+              highlight = highlightOptions(weight = 2, fillOpacity = 0.8, bringToFront = T),
+              popup = paste0("FIPS Code: ", cvilleshapes$GEOID, "<br>",
+                             "PM2.5 Level: ", cvilleshapes$PM25)) %>%
+  addLegend("bottomright", pal = pal, values = cvilleshapes$PM25,
+            title = "PM2.5 Concentrations", opacity = 0.7)
+# The trends on this metric are more similar to the replication data than on the percentile map
+  # However, PM2.5 concentrations are still much higher in Louisa County on this map than on the replication
+
+# Summary Table
+cville_area %>% select(-c(GEOID, STATE_NAME, B_DSLPM:T_PM25, NPL_CNT, TSDF_CNT)) %>% 
+  select(where(~is.numeric(.x) && !is.na(.x))) %>% 
+  as.data.frame() %>% 
+  stargazer(., type = "text", title = "Summary Statistics", digits = 0,
+            summary.stat = c("mean", "sd", "min", "median", "max"))
+
+# Visual Distributions
+# Ozone vs. PM25
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=OZONE, y=PM25))
+
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=P_OZONE, y=P_PM25))
+
+# Proximity to traffic vs. air toxics cancer risk
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=PTRAF, y=CANCER))
+
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=P_PTRAF, y=P_CANCR))
+# Interestingly, there is a correlation in the percentiles, but not on the actual levels
+
+# Proximity to traffic vs. diesel particulate matter level
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=PTRAF, y=DSLPM))
+
+cville_area %>% 
+  ggplot() +
+  geom_point(aes(x=P_PTRAF, y=P_DSLPM))
+# As above, there is a correlation in the percentiles, but not on the actual levels
+
 
 # PM2.5, ozone, and NATA indicators are measured at the census tract level
   # So each block group within that census tract is assigned the same value
-  # That means this isn't super useful for a lot of these measures (especially PM2.5)
-
-# Some questions that I have:
-# What do the NAs represent? Are they 0s? 
-  # NAs only exist for PTRAF and PWDIS
-  # In the case of traffic proximity, would that mean that there are no roads in the block group?
-  # I think NAs are probably 0s but I'm not completely sure
+  # That means this may not be super useful for a lot of these measures (especially PM2.5)
 
 
 # Comparison to PM2.5 data
 # Create tract variable
-cville_area %>% 
-  str_sub(GEOID, 1, 11)
+cville_area <- cville_area %>% 
+  mutate(tract = str_sub(GEOID, 1, 11))
 
 # Load data
 airquality <- read_csv("/Users/marisalemma/Desktop/Equity Center/summer-sandbox/cville_region_collection/data/airquality_cville_tract.csv")
+airquality_eastern <- read_csv("/Users/marisalemma/Desktop/Equity Center/summer-sandbox/eastern_shore_collection/data/airquality_eastern_tract.csv")
+
+# Merge datasets
+airquality <- airquality %>% rename(tract = CTIDFP00)
+
+pm25 <- cville_area %>% 
+  group_by(tract) %>% 
+  summarize(pm25_ejscreen = mean(PM25),
+            pctile_pm25_ejscreen = mean(P_PM25))
+
+pm25 <- merge(airquality, pm25, by = 'tract', all.x = T)
+
+# Scatterplots to see if trends between the two metrics are consistent
+pm25 %>% 
+  ggplot() +
+  geom_point(aes(x = PM2_5_2016, y = pm25_ejscreen))
+# There is some correlation between the two metrics, although not as much as I would have liked
+
+pm25 %>% 
+  ggplot() +
+  geom_point(aes(x = percentile_2016, y = pctile_pm25_ejscreen))
+# There is considerably less correlation between these two metrics. I am not sure why this is the case
+
+# Looking at differences between the two sets of values
+pm25 <- pm25 %>% 
+  mutate(pmdifference = pm25_ejscreen - PM2_5_2016,
+         pctiledifference = pctile_pm25_ejscreen - percentile_2016)
+# pmdifference is all over the place (some are positive, some are negative)
+# pctiledifference is all negative, but the values vary widely
+  # Some of the values are very large -- I'm not sure why this difference exists
+
+
+# Some questions that all this exploration raises:
+  # What do the NAs represent? Are they 0s? 
+    # NAs only exist for PTRAF and PWDIS
+    # In the case of traffic proximity, would that mean that there are no roads in the block group?
+    # I think NAs are probably 0s but I'm not completely sure
+  # Looking at the nationwide data, it seems like the percentiles don't make sense 
+    # For a lot of the percentile variables, 0 is not the lowest - not sure I understand why
+  # Why are there such large differences between the replication data estimates and the EJSCREEN estimates?
+    # Specifically for PM2.5, there are very large differences and I don't understand why
+
