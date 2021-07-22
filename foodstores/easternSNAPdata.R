c4cb6962f7fc5d16d64611cbe75346b93558e449
Khalila <kek7rdu@virginia.edu>
2021-07-22 15:13:43 -0400

    updating snap data

--- foodstores/easternSNAPdata.R
+++ foodstores/easternSNAPdata.R
@@ -16,20 +16,62 @@ easternstores_json <- fromJSON(eastern_path)
 # Extract data frame from list
 easternstores <- easternstores_json$features$attributes
 
+# make it an SF object, assign CRS
+easternstores_4326 <- st_as_sf(easternstores,
+                        coords = c("Longitude", "Latitude"),
+                        crs = 4326)
+
 # Filter to main counties
 easternCounties <- c("ACCOMACK", "NORTHAMPTON")
 
 easternstores <- easternstores %>% 
   filter(County %in% easternCounties)
 
-# Filtering to only grocery stores, supermarkets
+easternstores_4326 <- easternstores_4326 %>% 
+  filter(County %in% easternCounties)
+
+
+# Manually filtering to only grocery stores, supermarkets 
 # not convenience stories, pharmacies, dollar stores, warehouse clubs, etc.
 
-write_csv(easternstores, path = "rawEasternStores.csv")   # split screen / make counting easier
+write_csv(easternstores, path = "rawEasternStores.csv")   # just so i can split screen / make counting easier
 
-included <- easternstores[c(1, 14:15, 17:19, ), ] 
+included <- easternstores[c(1, 14:15, 17, 19:20, 33:34, 39:40, 46, 58:59, 65), ] 
 #14: mexican grocery store
 #15: el crucero-- fresh produce so including
+#34: general store but sells produce
+#65: including bc supermarket in the name, but store doesn't seem to exist? doesn't come up in google search (even when searching address)
 
-excluded <- easternstores[c(2:13, 16, 20:), ] # continue from 23
+excluded <- easternstores[c(2:13, 16, 18, 21:32, 35:38, 41:45, 47:57, 60:64), ] 
 #20: seafood only
+#23: el romelino-- general contracters? don't think they sell food of any kind
+#28: seafood only
+#42: ^
+
+
+# Reproducible Method
+
+storeNames <- dplyr::pull(easternstores, Store_Name) # creating vector to use in str_detect
+
+keyWords <- c("Enterprises|Markets|Dollar|Deli|Royal|Remolino|Variety|Seafood|
+              |Roses|Casa|Shore|Convenience|CVS|Goose|Walgreens|E & C")
+
+groceryOnly <- easternstores %>% 
+  filter(str_detect(storeNames, regex(keyWords, ignore_case = T), negate = T))
+
+groceryOnly[!groceryOnly$Store_Name%in%included$Store_Name,] # looks good :)
+
+# Save csv
+
+write_csv(groceryOnly, path = "snap_eastern.csv")
+
+
+# Map
+lon <- pull(groceryOnly, Longitude)
+lat <- pull(groceryOnly, Latitude)
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%  
+  setView(lng = mean(lon), lat = mean(lat), zoom = 9) %>% 
+  addMarkers(lng = lon, lat = lat, popup = groceryOnly$Store_Name,
+             clusterOptions = markerClusterOptions())
--- foodstores/roughWork.R
+++ foodstores/roughWork.R
@@ -5,79 +5,31 @@ library(ggplot2)
 library(ggmap)
 library(leaflet)
 
-write_csv(stores_4326, path = "rawCvilleStores.csv")   # split screen / make counting easier
-
-included <- stores_4326[c(4, 6, 10:13, 19:20, 33:34, 44:45, 47:49, 52:53, 56:58,
-                          65, 74, 76, 78, 80, 84, 88:89, 92, 94, 101, 105:107, 
-                          111:112, 114:115, 117, 125, 127, 144, 146, 156, 161:163,
-                          165, 167, 169), ] 
-#20: international food grocery stores so may not fit criteria of "wide variety" of healthy foods, but does for some people so going to include
-#44: african market place ^
-#49: Rebecca's Natural Food-- definitely not a large grocery store but it does provide healthy food options?
-#57: integral yoga-- same issue ^
-#65: asian market-- int'l food...
-#112: La Guadalupana ^
-#127: indian bazar-- int'l food
-#161: latin market...
-#169: indian & nepali
-
-
-excluded <- stores_4326[c(1:3, 5, 7:9, 14:18, 21:32, 35:43, 46, 50:51, 54:55, 59:64,
-                          66:73, 75, 77, 79, 81:83, 85:87, 90:91, 93, 95:100, 102:104,
-                          108:110, 113, 116, 118:124, 126, 128:143, 145, 147:155,
-                          157:160, 164, 166, 168, 170:171), ]
-#16: scottsville farmers market only open 9am-1pm on saturdays -- not accessible
-#28: bakery
-#38: greene farmers market only open sat 8am-12pm -- not accessible
-#82: sam's club-- wholesale club
-#103: market street market-- seems niche: cheeses and beers
-#109: costco--wholesale club
-#117: aghan grand market-- int'l food but seems too small to actually be very useful
-      # exclude african market on same premise?
-#118: Mineral Farmer's Market -- inaccessible hours
-#149: big lots-- no fresh produce
-#151: market central-- Saturdays 8:00 am-12:00 pm .. not accessible
-
-# if not mentioned by row specifically, it fell into category of convenience, dollar store, gas station, etc.
-
-
-# reproducible method
-keyWords <- c("Lion|Kroger|Whole Foods|Reid|Hilltop|Tio|Depot|African|Trader|Natural|
-              |Millers|Teeter|Yoga|Valu|Asian|Aldi|Medina|Target|Blue|Nx|Thomas INC|
-              |Caul’s|Guadalupana|Mercado|Oriental|Walmart|Indian|Giant|Latino|Lidl|
-              |amores|Wegmans|Express Grocery|Caul")
-
-storeNames <- dplyr::pull(stores, Store_Name) # creating vector to use in str_detect
-
-groceryOnly <- stores %>% 
-  filter(str_detect(storeNames, regex(keyWords, ignore_case = T), negate = F))
-
-groceryOnly[!groceryOnly$Store_Name%in%included$Store_Name,] # looks good :)
-
 
 # Maps ... :(
 
 # creating a sample data.frame with your lat/lon points
 lon <- pull(groceryOnly, Longitude)
 lat <- pull(groceryOnly, Latitude)
-storeName <- pull(groceryOnly, Store_Name)
-county <- pull(groceryOnly, County)
 
 df <- as.data.frame(cbind(lon,lat))
 
 
 # getting the map
-mapgilbert <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 11,
-                      maptype = "satellite")
-view(mapgilbert)
+map <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 9,
+                      maptype = "roadmap")
+view(map)
 
 # plotting the map with some points on it
-ggmap(mapgilbert) +
-  geom_point(data = df, aes(x = lon, y = lat, fill = "red", alpha = 0.8))
+ggmap(map) +
+  geom_point(data = df, aes(x = lon, y = lat, col = "green3", fill = "green2", alpha = 0.8))
+
+
+pal <- colorNumeric("plasma", reverse = TRUE, domain = groceryOnly$Longitude, groceryOnly$Latitude) # viridis
 
 leaflet() %>% 
   addProviderTiles("CartoDB.Positron") %>% 
-  addPolygons(data = df,
+  addPolygons(data = map,
               fillColor = ~pal(storeNames),
               weight = 1,
               opacity = 1,
@@ -93,4 +45,13 @@ leaflet() %>%
   ) %>% 
   addLegend("bottomright", pal = pal, values = groceryOnly$Store_Name, 
             title = "# of Grocery Stores", opacity = 0.7)
+?popupOptions
+
+PopUpInfo <- paste(groceryOnly[c("Store_Name", "Address", "City")], sep = "/n", collapse = "")
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%  
+  setView(lng = mean(lon), lat = mean(lat), zoom = 8) %>% 
+  addMarkers(lng = lon, lat = lat, popup = groceryOnly$Store_Name) %>% 
+  markerOptions(riseOnHover = T)
 
