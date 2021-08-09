ed0d63b9cf736744c2e4a94480ba2e76b8916f69
Khalila <kek7rdu@virginia.edu>
2021-08-09 13:06:00 -0400

    infogroup update

--- foodstores/roughWork.R
+++ foodstores/roughWork.R
@@ -8,14 +8,16 @@ library(leaflet)
 # Charlottesville
 
 #12: ahart's market?
-#25: Blue Ridge Country Store?-- country store but kind of a deli
 #34: Charlottesville Oriental Food?
-#119: Graves Grocery?-- outside gas station but says grocery on sign
 #225: William's Grocery?
 
 #42: Corner Market & More
 #121: Greenwood Gourmet Grocery
-#152: Food Pantry (open limited hours 3x week)
-#202: Target
+
 
 # Eastern Shore
+
+#27: El Milagro Markets?
+#28: El Ranchito Mexican Store?
+
+#51: Poseidon's Pantry
