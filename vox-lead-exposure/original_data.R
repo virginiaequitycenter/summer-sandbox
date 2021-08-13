e5fd9a17ab290df676fdca41cb24b497a91cd617
molemma <ml2cc@virginia.edu>
2021-08-13 09:19:43 -0400

    lead exposure updates

--- vox-lead-exposure/original_data.R
+++ vox-lead-exposure/original_data.R
@@ -4,65 +4,54 @@
 # Age of the buildings (indicating likelihood of exposure to lead paint) and poverty levels were found to be the biggest indicators of lead exposure, which is why they look specifically at those
 
 library(tidyverse)
+library(leaflet)
+library(sf)
+library(tidycensus)
 
 # import data
-# data came from US Census Bureau
-housing <- read_csv("vox-lead-exposure/housing_data_2014.csv")
-poverty <- read_csv("vox-lead-exposure/poverty_data_2014.csv")
+poverty <- get_acs(geography = "tract",
+                   state = c("01":"02", "04":"06", "08":"10", "12":"13", "15":"42", "44":"51", "53":"56"),
+                   variables = c("S1701_C01_001", "S1701_C01_039"),
+                   year = 2014,
+                   output = "wide")
 
-# delete second header row
-housing <- housing [-c(1), ]
-poverty <- poverty [-c(1), ]
+housing <- get_acs(geography = "tract",
+                   state = c("01":"02", "04":"06", "08":"10", "12":"13", "15":"42", "44":"51", "53":"56"),
+                   table = "B25034",
+                   year = 2014,
+                   output = "wide")
 
-# remove census tracts with no data and census tracts in Puerto Rico
+# remove census tracts with no data
 housing <- housing %>% 
-  filter(HD01_VD01!=0) %>% 
-  filter(!grepl('Puerto Rico', `GEO.display-label`))
+  filter(B25034_001E!=0)
 poverty <- poverty %>% 
-  filter(HC01_EST_VC01!=0) %>% 
-  filter(!grepl('Puerto Rico', `GEO.display-label`))
-
-# convert character to numeric
-housing$HD01_VD01 <- as.numeric(housing$HD01_VD01)
-housing$HD01_VD02 <- as.numeric(housing$HD01_VD02)
-housing$HD01_VD03 <- as.numeric(housing$HD01_VD03)
-housing$HD01_VD04 <- as.numeric(housing$HD01_VD04)
-housing$HD01_VD05 <- as.numeric(housing$HD01_VD05)
-housing$HD01_VD06 <- as.numeric(housing$HD01_VD06)
-housing$HD01_VD07 <- as.numeric(housing$HD01_VD07)
-housing$HD01_VD08 <- as.numeric(housing$HD01_VD08)
-housing$HD01_VD09 <- as.numeric(housing$HD01_VD09)
-housing$HD01_VD10 <- as.numeric(housing$HD01_VD10)
+  filter(S1701_C01_001E!=0)
 
 # create age categories (these essentially represent number of houses in tract that are at lead risk)
 housing <- housing %>% 
-  mutate(age_39 = HD01_VD10 * 0.68,
-         age40_59 = (HD01_VD09+HD01_VD08)*0.43,
-         age60_79 = (HD01_VD07+HD01_VD06)*0.08,
-         age80_99 = (HD01_VD05+HD01_VD04)*0.03,
-         age00_10 = (HD01_VD03+HD01_VD02)*0,
-         total = HD01_VD01)
+  mutate(age_39 = B25034_010E * 0.68,
+         age40_59 = (B25034_009E+B25034_008E)*0.43,
+         age60_79 = (B25034_007E+B25034_006E)*0.08,
+         age80_99 = (B25034_005E+B25034_004E)*0.03,
+         age00_10 = (B25034_003E+B25034_002E)*0,
+         total = B25034_001E)
 
 # calculate housing risk for census tract
 housing <- housing %>% 
   mutate(sum_housing_risk = age_39 + age40_59 + age60_79 + age80_99 + age00_10,
          housing_risk = (sum_housing_risk/total)*100)
 
-# convert character to numeric (poverty)
-poverty$HC01_EST_VC01 <- as.numeric(poverty$HC01_EST_VC01)
-poverty$HC01_EST_VC49 <- as.numeric(poverty$HC01_EST_VC49)
-
 # calculate poverty risk for census tract
 poverty <- poverty %>% 
-  rename(total_under_125pct = HC01_EST_VC49,
-         total_poverty_status = HC01_EST_VC01) %>% 
+  rename(total_under_125pct = S1701_C01_039E,
+         total_poverty_status = S1701_C01_001E) %>% 
   mutate(poverty_risk = (total_under_125pct/total_poverty_status)*100)
 
 # filter to just variables of interest
 housing <- housing %>% 
-  select("GEO.id":"GEO.display-label", "age_39":"housing_risk")
+  select("GEOID":"NAME", "age_39":"housing_risk")
 poverty <- poverty %>% 
-  select("GEO.id":"GEO.display-label", "total_poverty_status", "total_under_125pct", "poverty_risk")
+  select("GEOID":"NAME", "total_poverty_status", "total_under_125pct", "poverty_risk")
 
 # merge two datasets together
 combined_risk <- inner_join(housing, poverty)
@@ -86,19 +75,15 @@ combined_risk <- combined_risk %>%
   mutate(lead_risk_rank = ntile(leadriskscore_raw, 10))
 
 # filter to just Cville region
-combined_risk <- combined_risk %>% 
-  mutate(GEO.id = str_sub(GEO.id, 10, 20))
-
 cville_area <- combined_risk %>% 
-  filter(GEO.id%in%51003010100:51003011400 | 
-           GEO.id%in%51065020101:51065020300 | 
-           GEO.id%in%51079030101:51079030200 |
-           GEO.id%in%51109950100:51109950500 | 
-           GEO.id%in%51125950100:51125950300 | 
-           GEO.id%in%51540000201:51540001000)
+  filter(GEOID%in%51003010100:51003011400 | 
+           GEOID%in%51065020101:51065020300 | 
+           GEOID%in%51079030101:51079030200 |
+           GEOID%in%51109950100:51109950500 | 
+           GEOID%in%51125950100:51125950300 | 
+           GEOID%in%51540000201:51540001000)
 
 cville_shapes <- readRDS("cville_region_collection/data/cville_tracts.RDS")
-cville_area <- cville_area %>% rename(GEOID = GEO_ID)
 leadrisk <- merge(cville_shapes, cville_area, by = 'GEOID', all.x = TRUE)
 leadrisk <- st_transform(leadrisk, crs = 4326)
 
