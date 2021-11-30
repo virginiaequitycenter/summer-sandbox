f98cde5e57924aa210b65a1a77941d1795c40d69
Michele Claibourn <mclaibourn@virginia.edu>
2021-11-29 21:08:51 -0500

    update air quality files

--- eastern_shore_collection/README.md
+++ eastern_shore_collection/README.md
@@ -1,30 +1,38 @@
 # Eastern Shore Data Collection
 
-Building a data collection for Eastern Shore climate/environmental justice platform
+Building a data collection for Eastern Shore climate justice work/platform.
 
 ## Data Descriptions
 
-### Events/Physical
-
-* [Natural hazard risk/FEMA's National Risk Index](https://virginiaequitycenter.github.io/summer-sandbox/fema_nri_eastern.html)
-* [NOAA County-level Temperature over Time](https://virginiaequitycenter.github.io/summer-sandbox/noaa_eastern.html)
+### Climate Measures
+* [Daymet Summer/Annual Maximum Temperature and Precipitation](https://virginiaequitycenter.github.io/summer-sandbox/daymet_cville.html)
 * [Air Quality (Replication Data) - Tract level](https://virginiaequitycenter.github.io/summer-sandbox/airquality_eastern.html)
-* [FEMA National Flood Insurance Program Claims](https://virginiaequitycenter.github.io/summer-sandbox/femaclaims_eastern.html)
-* [NLCD 2016 Tree Canopy](https://virginiaequitycenter.github.io/summer-sandbox/nlcd_tree_cville_eastern.html)
-* [EJSSCREEN Environmental Indicators](https://virginiaequitycenter.github.io/summer-sandbox/ejscreen_eastern.html)
-* [FCC Broadband Availability - Block level](https://virginiaequitycenter.github.io/summer-sandbox/fcc_eastern_blocks.html)
-* [FCC Broadband Availability - Block Group level](https://virginiaequitycenter.github.io/summer-sandbox/fcc_eastern_blkgps.html)
-* [FCC Broadband Availability - Tract level](https://virginiaequitycenter.github.io/summer-sandbox/fcc_eastern_tracts.html)
 * [LANDSAT8 Surface Temperature - (in progress)](https://virginiaequitycenter.github.io/summer-sandbox/landsat8_explore.html)
+* [NOAA County-level Temp/Precipitation](https://virginiaequitycenter.github.io/summer-sandbox/noaa_cville.html)
 
-### People/Economic
 
-* [Low-Income Energy Affordability Data](https://virginiaequitycenter.github.io/summer-sandbox/lead_eastern.html)
-* [LEHD Origin-Destination Employment Statistics (LODES) - Block level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_eastern_blocks.html)
-* [LEHD Origin-Destination Employment Statistics (LODES) - Block Group level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_eastern_blkgr.html)
-* [LEHD Origin-Destination Employment Statistics (LODES) - Tract level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_eastern_tract.html)
-* [LEHD Origin-Destination Commutes - Block level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_eastern_block.html)
-* [LEHD Origin-Destination Commutes - Block Group level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_eastern_blkgp.html)
-* [LEHD Origin-Destination Commutes - Tract level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_eastern_tract.html)
-* [Food Retailers Locations](https://virginiaequitycenter.github.io/summer-sandbox/SNAP_eastern.html)
-* [Walkability Indicators](https://virginiaequitycenter.github.io/summer-sandbox/Walkability.html)
+### Hazards and Risks
+* [National Flood Hazard Layer's 1% and 2% annual floodplains](https://virginiaequitycenter.github.io/summer-sandbox/nfhl_cville.html)
+* [Natural hazard risk/FEMA's National Risk Index](https://virginiaequitycenter.github.io/summer-sandbox/fema_nri_cville.html)
+* [FEMA National Flood Insurance Program Claims](https://virginiaequitycenter.github.io/summer-sandbox/femaclaims_cville.html)
+* [EJSCREEN Environmental Indicators](https://virginiaequitycenter.github.io/summer-sandbox/ejscreen_cville.html)
+* [Estimated Lead Exposure Risk](https://virginiaequitycenter.github.io/summer-sandbox/lead_exposure_cville.html)
+* [CDC PLACES Health Outcomes](https://virginiaequitycenter.github.io/summer-sandbox/cdcplaces_cville.html)
+
+
+### Community Assets and Infrastructure
+* [NLCD - Land Cover, Tree Canopy, Impervious Surfaces](https://virginiaequitycenter.github.io/summer-sandbox/nlcd_cville.html)
+* [Food Retailer Locations](https://virginiaequitycenter.github.io/summer-sandbox/food_retail_cville.html)
+* [Low-Income Energy Affordability Data](https://virginiaequitycenter.github.io/summer-sandbox/lead_cville.html)
+* [FCC Broadband Availability](https://virginiaequitycenter.github.io/summer-sandbox/fcc_cville.html)
+* LEHD Origin-Destination Employment Statistics (LODES)
+    - Block level, [Block Group level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_cville_blkgr.html), [Tract level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_cvl_tract.html)
+* [Home Mortgage Disclosure Act Data](https://virginiaequitycenter.github.io/summer-sandbox/hmda_cville_tract.html)
+
+### Transportation
+* [Walkability Indicators](https://virginiaequitycenter.github.io/summer-sandbox/walkability_cville.html)
+* LEHD Origin-Destination Commutes
+  - [Block level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_cville_block.html), [Block Group level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_cville_blkgp.html), [Tract level](https://virginiaequitycenter.github.io/summer-sandbox/lodes_commute_cville_tract.html)
+
+### People
+* [American Community Survey measures](https://virginiaequitycenter.github.io/summer-sandbox/Population-data-table.html)
--- eastern_shore_collection/scripts/airquality.R
+++ eastern_shore_collection/scripts/airquality.R
@@ -1,84 +1,210 @@
-# Air Quality Data Exploration
-# Marisa Lemma
-# 7/1/21
+# Get Census Tract Air Quality Data
+# Marisa Lemma, Michele Claibourn
+# Created: 2021-07-01
+# Last updated: 2021-11-29
 
-library(haven) #to import .dta
+library(haven) # to import .dta
 library(tidyverse)
+library(sf)
+library(tigris)
+library(areal) # to interpolate 2000 tracts to 2010 tracts
 
-# Import data from source
-leadurl <- "https://dataverse.lib.virginia.edu/api/access/datafile/24474"
-download.file(url = leadurl,
-              destfile = paste(getwd(), "/", "airpollution.zip", sep = ""))
-unzip("airpollution.zip", exdir = paste(getwd(), "/data/", sep = ""))
-
-masterdata <- read_dta("data/Master_Data.dta")
-
-# Adjust units for PM2.5
-masterdata <- masterdata %>% 
-  mutate(CorrectedPM2_5_ = (CorrectedPM2_5_/100))
-
-# Drop unnecessary variables
-masterdata <- masterdata %>% 
-  select(CTIDFP00, COUNTYFP00, NAME00, CorrectedPM2_5_, percentile)
-
-# Filter by year
-master_1981 <- masterdata %>% 
-  filter(Year=="1981")
-master_2016 <- masterdata %>% 
-  filter(Year=="2016")
-
-# Rename PM2.5 values
-master_1981 <- master_1981 %>% 
-  rename(PM2_5_1981 = CorrectedPM2_5_)
-master_2016 <- master_2016 %>% 
-  rename(PM2_5_2016 = CorrectedPM2_5_)
-
-# Prepare data frames for merge
-master_1981$Year <- NULL
-master_2016$Year <- NULL
-
-# Merge datasets
-airquality <- inner_join(master_1981, master_2016)
-
-# Export to csv to use in Stata (to convert to 2010 tracts)
-write.csv(airquality, "~/airquality-replication/airquality_tract.csv", row.names = F)
-# Bridging data from https://s4.ad.brown.edu/Projects/Diversity/Researcher/Bridging.htm
-
-# Import new dataset with 2010 census tract data (from Stata)
-airquality_2010 <- read_dta("~/airquality-replication/airquality_2000-2010.dta")
-
-# Create percentiles for each year
-airquality_2010 <- airquality_2010 %>% 
-  mutate(percentile_1981 = percent_rank(pm2_5_1981),
-         percentile_2016 = percent_rank(pm2_5_2016))
-airquality_2010 <- airquality_2010 %>% 
-  mutate(percentile_1981 = (percentile_1981*100),
-         percentile_2016 = (percentile_2016*100))
-# Is there a way to round these percentile scores?
-# Also: when they calculated percentiles, they also made one that was weighted. Not sure if/how to do that
-
-airquality_2010 <- airquality_2010 %>% 
-  mutate(STATEFP00 = str_sub(trtid10, 1, 2),
-         COUNTYFP00 = str_sub(trtid10, 3, 5))
 
 # Define area of interest
-easternshore_fips <- c("001", "131")
+# Cville area
+# localfips <- c("540", "003", "065", "079", "109", "125")
+# Eastern shore area
+localfips <- c("001", "131")
+
+# Import data from source ----
+# if (!dir.exists("dataraw")) {dir.create("dataraw")}
+# 
+# url <- "https://dataverse.lib.virginia.edu/api/access/datafile/24474"
+# download.file(url = url,
+#               destfile = paste(getwd(), "/dataraw/", "airpollution.zip", sep = ""))
+# unzip("dataraw/airpollution.zip", exdir = paste(getwd(), "/dataraw/airpollution/", sep = ""))
+# 
+# Read in Stata dataset
+wide <- read_dta("dataraw/airpollution/Master_Data_Wide.dta")
+
+
+# Read in 2000 and 2010 tracts ----
+# We need the data in 2010 census tracts, but here it is in 2000 tracts
+us_states <- unique(fips_codes$state)[1:51]
+continental_states <- us_states[!us_states %in% c("AK", "HI")]
+us_tracts <- rbind_tigris(
+  lapply(
+    continental_states, function(x) {
+      tracts(state = x, cb = TRUE, year = 2010, class = "sf")
+    }
+  )
+)
+
+us_tracts_2000 <- rbind_tigris(
+  lapply(
+    continental_states, function(x) {
+      tracts(state = x, cb = TRUE, year = 2000, class = "sf")
+    }
+  )
+)
+
+# need a planar CRS (not lat/lon) for interpolation
+us_tracts_3857 <- st_transform(us_tracts, crs = 3857)
+us_tracts_2000_3857 <- st_transform(us_tracts_2000, crs = 3857)
+
+# Join data to 2000 tracts
+# create gid in both
+us_tracts_2000_3857 <- us_tracts_2000_3857 %>% 
+  mutate(sid = paste0(STATEFP, COUNTYFP, TRACT))
+
+us_tracts_3857 <- us_tracts_3857 %>% 
+  mutate(tid = paste0(STATEFP, COUNTYFP, TRACT))
+
+us_tracts_2000_3857_aq <- us_tracts_2000_3857 %>% 
+  left_join(wide, by = c("sid" = "CTIDFP00")) 
+
+# save image here so we don't have to restart if
+# process below terminates!
+save.image("dataraw/airpollution/crosswalksteps.Rdata")
+
+
+# load("dataraw/airpollution/crosswalksteps.Rdata")
+
+# ..................................
+# Areal interpolation, US ----
+# of 2000 tract AQ data to 2010 tract
+# all PM25 vars
+
+vlist <- wide %>% 
+  select(contains("Corrected")) %>% 
+  names()
+
+# check
+ar_validate(target = us_tracts_3857, source = us_tracts_2000_3857_aq, 
+            varList = vlist, 
+            method = "aw", verbose = TRUE)
+
+# interpolate AQ to 2010 shape files
+us_tracts_2010_aq <- aw_interpolate(us_tracts_3857, 
+                                    tid = tid,
+                                    source = us_tracts_2000_3857_aq, 
+                                    sid = sid,
+                                    weight = "sum", 
+                                    output = "tibble",
+                                    intensive = vlist)
+
+
+# Rename and process ----
+airquality <- us_tracts_2010_aq %>% 
+  # Rename (remove Corrected)
+  rename_with(~str_remove(., pattern = "Corrected"), everything()) %>% 
+  rename_with(tolower, everything()) %>% 
+  # Adjust units for PM2.5
+  mutate(across(contains("pm2_5"), ~ .x/100)) %>% 
+  # Recover state and county fips
+  mutate(STATEFP00 = str_sub(trtid10, 1, 2),
+         COUNTYFP00 = str_sub(trtid10, 3, 5)) %>% 
+  # Create percentiles for each year
+  mutate(percentile_1981 = round(percent_rank(pm2_5_1981)*100,1),
+         percentile_2016 = round(percent_rank(pm2_5_2016)*100,1)) %>% 
+  # Create variables for change in PM2.5 levels
+  mutate(pm_change = (pm2_5_2016 - pm2_5_1981),
+         pctile_change = (percentile_2016 - percentile_1981))
+
+# When authors calculated percentiles, they included one that was 
+# weighted. We don't do that here.
 
 # Filter to area of interest
-virginia <- airquality_2010 %>% 
-  filter(STATEFP00=="51")
-easternshore <- virginia %>% 
-  filter(COUNTYFP00%in%easternshore_fips)
+eastern_area <- airquality %>% 
+  filter(STATEFP00=="51" & COUNTYFP00 %in% localfips)
 
-# Create variable for change in PM2.5 levels
-airquality <- easternshore %>% 
-  mutate(PM_change = (pm2_5_2016 - pm2_5_1981))
-# All these values are negative - should we swap 1981 and 2016 so that all the values are positive?
+# Export to CSV ----
+write_csv(eastern_area, "eastern_shore_collection/data//airquality_eastern_tract.csv", row.names = F)
 
-# Create variable for change in percentile
-airquality <- airquality %>% 
-  mutate(pctile_change = (percentile_2016 - percentile_1981))
-# These values are also negative
 
-# Export to CSV
-write.csv(airquality, "eastern_shore_collection/data//airquality_eastern_tract.csv", row.names = F)
+# ..................................
+# Areal Interpolation, VA ----
+# The first attempt at interpolating generated erros; 
+# step through with reduced set to diagnose
+
+# # Just VA, just 2016 pm25
+va_tracts_3857 <- us_tracts_3857 %>%
+  filter(STATEFP == "51")
+
+va_tracts_2000_3857_aq <- us_tracts_2000_3857_aq %>%
+  filter(STATEFP == "51")
+
+# check
+ar_validate(target = va_tracts_3857, source = va_tracts_2000_3857_aq,
+            varList = "CorrectedPM2_5_2016",
+            method = "aw", verbose = TRUE)
+
+# # Intersect data
+# intersect <- va_tracts_3857 %>%
+#   aw_intersect(source = va_tracts_2000_3857_aq, areaVar = "area")
+# # intersect
+# 
+# # Calculate total area
+# intersect <- intersect %>%
+#   aw_total(source = va_tracts_2000_3857_aq, id = sid, areaVar = "area", 
+#            totalVar = "totalArea", type = "intensive", weight = "sum") 
+# # intersect
+# 
+# # Calculate areal weight
+# intersect <- intersect %>%
+#   aw_weight(areaVar = "area", totalVar = "totalArea",
+#             areaWeight = "areaWeight") 
+# # intersect
+# 
+# # Calculate estimates
+# intersect <- intersect %>%
+#   aw_calculate(value = vlist, areaWeight = "areaWeight") 
+# # intersect
+# 
+# # Aggregate by target id
+# result <- intersect %>%
+#   aw_aggregate(target = va_tracts_3857, tid = tid, interVar = CorrectedPM2_5_2016)
+# result
+# 
+# ggplot(data = result) +
+#   geom_sf(aes(fill = CorrectedPM2_5_2016)) +
+#   theme_void()
+
+# All together (for VA only)
+va_tracts_2010_aq <- aw_interpolate(va_tracts_3857, 
+                                    tid = tid,
+                                    source = va_tracts_2000_3857_aq, 
+                                    sid = sid,
+                                    weight = "sum", 
+                                    output = "tibble",
+                                    intensive = vlist)
+# Works!
+
+# Rename and process ----
+va_airquality <- va_tracts_2010_aq %>% 
+  # Rename (remove Corrected)
+  rename_with(~str_remove(., pattern = "Corrected"), everything()) %>% 
+  rename_with(tolower, everything()) %>% 
+  # Adjust units for PM2.5
+  mutate(across(contains("pm2_5"), ~ .x/100)) %>% 
+  # Create percentiles for each year
+  mutate(percentile_1981 = round(percent_rank(pm2_5_1981)*100,1),
+         percentile_2016 = round(percent_rank(pm2_5_2016)*100,1)) %>% 
+  # Create variables for change in PM2.5 levels
+  mutate(pm_change_1981_2016 = (pm2_5_2016 - pm2_5_1981),
+         pctile_change_1981_2016 = (percentile_2016 - percentile_1981))
+
+# Filter to area of interest and select vars
+# cville_area <- va_airquality %>% 
+#   filter(countyfp %in% localfips) %>% 
+#   select(statefp, countyfp, tract, gid = tid, pm2_5_1981:pctile_change_1981_2016)
+
+eastern_area <- va_airquality %>%
+filter(countyfp %in% localfips) %>%
+  select(statefp, countyfp, tract, gid = tid, pm2_5_1981:pctile_change_1981_2016)
+
+
+# Export to CSV ----
+# write_csv(cville_area, "data/airquality_cville_tract.csv")
+
+write_csv(eastern_area, "data/airquality_eastern_tract.csv")
